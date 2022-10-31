6aaf5df2395c8a2b3c008a73b0401279431edbde
xxh205 <masonhuo88@gmail.com>
2022-10-31 16:20:26 +0000

    leetcode

---
+++ src/main/java/Array/SevenHundredFour_BinarySearch.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class SevenHundredFour_BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        System.out.println(search(nums,target));
+    }
+
+    public static int search(int[] nums, int target) {
+        int left = 0, right = nums.length-1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+}
+
