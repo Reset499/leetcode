7a51878623b7c0c8699d5230ff09c95a2772fdde
xxh205 <masonhuo88@gmail.com>
2022-11-03 17:32:58 +0000

    leetcode

---
+++ src/main/java/LinkedList/TwoHundredTwo_reverseList.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class TwoHundredTwo_reverseList {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode temp = null;
+        while (cur != null) {
+            temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
