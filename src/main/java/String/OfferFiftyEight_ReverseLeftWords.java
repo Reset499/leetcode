7e8966025c29858a2f9f2624f5e55b3edefb8a2c
xxh205 <masonhuo88@gmail.com>
2022-11-10 16:39:16 +0000

    leetcode

---
+++ src/main/java/String/OfferFiftyEight_ReverseLeftWords.java
@@ -0,0 +1,25 @@
+package String;
+
+public class OfferFiftyEight_ReverseLeftWords {
+    public static void main(String[] args) {
+        System.out.println(reverseLeftWords("abcdefg", 2));
+    }
+
+    public static String reverseLeftWords(String s, int n) {
+        int len = s.length();
+        StringBuilder sb = new StringBuilder(s);
+        reverseString(sb, 0, n - 1);
+        reverseString(sb, n, len - 1);
+        return sb.reverse().toString();
+    }
+
+    public static void reverseString(StringBuilder sb, int start, int end) {
+        while (start < end) {
+            char temp = sb.charAt(start);
+            sb.setCharAt(start, sb.charAt(end));
+            sb.setCharAt(end, temp);
+            start++;
+            end--;
+        }
+    }
+}
