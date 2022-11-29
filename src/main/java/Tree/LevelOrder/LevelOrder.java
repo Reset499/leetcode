1f272afb08a4af10914523d94795a7a7ce0cbc3c
xxh205 <masonhuo88@gmail.com>
2022-11-29 19:41:30 +0000

    leetcode

---
+++ src/main/java/Tree/LevelOrder/LevelOrder.java
@@ -0,0 +1,31 @@
+package Tree.LevelOrder;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrder {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int len = queue.size();
+            while (len != 0) {
+                TreeNode tempNode = queue.poll();
+                list.add(tempNode.val);
+                if (tempNode.left != null) queue.offer(tempNode.left);
+                if (tempNode.right != null) queue.offer(tempNode.right);
+                len--;
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
