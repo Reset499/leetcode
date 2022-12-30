4819331c663b4eef4cc2487f218ba9c7b51bebc7
xxh205 <hello@itcast.cn>
2022-12-30 19:04:20 +0000

    leetcode

---
+++ src/main/java/Tree/SevenHundred_SearchBST.java
@@ -0,0 +1,13 @@
+package Tree;
+
+public class SevenHundred_SearchBST {
+    public static void main(String[] args) {
+
+    }
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null||root.val == val) return root;
+        if(val > root.val) root = searchBST(root.right,val);
+        else if(val < root.val) root = searchBST(root.left,val);
+        return root;
+    }
+}
