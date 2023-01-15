db0a530d93d28823f3bfa35fdb1fdc5d24a12f84
xxh205 <hello@itcast.cn>
2023-01-15 20:15:50 +0000

    leetcode

---
+++ src/main/java/BackTacking/FourHundredNinetyOne_FindSubsequences.java
@@ -0,0 +1,37 @@
+package BackTacking;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class FourHundredNinetyOne_FindSubsequences {
+    public static void main(String[] args) {
+
+    }
+
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        for (int i = 2; i <= nums.length; i++) {
+            backTracing(nums, i, 0,0);
+        }
+        return result;
+    }
+
+    public void backTracing(int[] nums, int k, int startIndex,int max) {
+        Map<Integer, Integer> map = new HashMap<>();
+        if (path.size() == k) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = startIndex; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) || nums[i] < max) continue;
+            path.add(nums[i]);
+            map.put(nums[i], 0);
+            max = nums[i];
+            backTracing(nums, k, i + 1,max);
+            path.removeLast();
+        }
+    }
+}
