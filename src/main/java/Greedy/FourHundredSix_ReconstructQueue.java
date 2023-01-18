0b7897d03e82f1b89a8dec4c184c7e70c6b120be
xxh205 <hello@itcast.cn>
2023-01-18 23:20:59 +0000

    leetcode

---
+++ src/main/java/Greedy/FourHundredSix_ReconstructQueue.java
@@ -0,0 +1,25 @@
+package Greedy;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class FourHundredSix_ReconstructQueue {
+    public static void main(String[] args) {
+
+    }
+    public int[][] reconstructQueue(int[][] people) {
+        // 身高从大到小排（身高相同k小的站前面）
+        Arrays.sort(people, (a, b) -> {
+            if (a[0] == b[0]) return a[1] - b[1];
+            return b[0] - a[0];
+        });
+
+        LinkedList<int[]> que = new LinkedList<>();
+
+        for (int[] p : people) {
+            que.add(p[1],p);
+        }
+
+        return que.toArray(new int[people.length][]);
+    }
+}
