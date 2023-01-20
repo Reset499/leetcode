232d7edb87cf5442c58c7285ca3c52bedc515e27
xxh205 <hello@itcast.cn>
2023-01-20 22:44:38 +0000

    leetcode

---
+++ src/main/java/Greedy/SevenHundredThirtyEight_MonotoneIncreasingDigits.java
@@ -0,0 +1,21 @@
+package Greedy;
+
+public class SevenHundredThirtyEight_MonotoneIncreasingDigits {
+    public static void main(String[] args) {
+    }
+    public  int monotoneIncreasingDigits(int n) {
+        String s = String.valueOf(n);
+        char[] chars = s.toCharArray();
+        int start = s.length();
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (chars[i] > chars[i + 1]) {
+                chars[i]--;
+                start = i+1;
+            }
+        }
+        for (int i = start; i < s.length(); i++) {
+            chars[i] = '9';
+        }
+        return Integer.parseInt(String.valueOf(chars));
+    }
+}
