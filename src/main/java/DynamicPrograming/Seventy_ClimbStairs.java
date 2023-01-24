1ca4d0670d62f534d64a5206c8debe64268211fd
xxh205 <hello@itcast.cn>
2023-01-24 23:17:49 +0000

    leetcode

---
+++ src/main/java/DynamicPrograming/Seventy_ClimbStairs.java
@@ -0,0 +1,16 @@
+package DynamicPrograming;
+
+public class Seventy_ClimbStairs {
+    public static void main(String[] args) {
+
+    }
+
+    public int climbStairs(int n) {
+        if (n < 2) return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) dp[i] = dp[i-1]+dp[i-2];
+        return dp[n];
+    }
+}
