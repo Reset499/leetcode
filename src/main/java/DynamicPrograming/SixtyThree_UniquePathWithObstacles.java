0d8712069689b9c0c75360b511e21cb6cb14ede5
xxh205 <hello@itcast.cn>
2023-01-25 17:07:49 +0000

    leetcode

--- src/main/java/DynamicPrograming/SixtyThree_UniquePathWithObstacles.java
+++ src/main/java/DynamicPrograming/SixtyThree_UniquePathWithObstacles.java
@@ -10,23 +10,11 @@ public class SixtyThree_UniquePathWithObstacles {
         int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                    continue;
-                }
-                if (i - 1 < 0) {
-                    dp[i][j] = dp[i][j - 1];
-                    continue;
-                }
-                if (j - 1 < 0) {
-                    dp[i][j] = dp[i - 1][j];
-                    continue;
-                }
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                if (obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else if (i == 0 && j == 0) dp[i][j] = 1;
+                else if (i - 1 < 0) dp[i][j] = dp[i][j - 1];
+                else if (j - 1 < 0) dp[i][j] = dp[i - 1][j];
+                else dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
             }
         }
         return dp[m - 1][n - 1];
