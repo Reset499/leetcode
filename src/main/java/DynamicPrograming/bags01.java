246d6db9ac7735e804283ca53ba052bb5d34bd97
xxh205 <hello@itcast.cn>
2023-01-26 17:16:55 +0000

    leetcode

--- src/main/java/DynamicPrograming/bags01.java
+++ src/main/java/DynamicPrograming/bags01.java
@@ -7,17 +7,28 @@ public class bags01 {
         System.out.println(bags01(weight, value, 4));
     }
 
+    //    public static int bags01(int[] weight, int[] value, int bagSize) {
+//        int m = weight.length;
+//        int n = bagSize;
+//        int[][] dp = new int[m][n + 1];
+//        for (int i = weight[0]; i < n + 1; i++) dp[0][i] = value[0];
+//        for (int i = 1; i < m; i++) {
+//            for (int j = 1; j < n + 1; j++) {
+//                if(j<weight[i]) dp[i][j] = dp[i-1][j];
+//                else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+//            }
+//        }
+//        return dp[m - 1][n];
+//    }
     public static int bags01(int[] weight, int[] value, int bagSize) {
-        int m = weight.length;
-        int n = bagSize;
-        int[][] dp = new int[m][n + 1];
-        for (int i = weight[0]; i < n + 1; i++) dp[0][i] = value[0];
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                if(j<weight[i]) dp[i][j] = dp[i-1][j];
-                else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+        int[] dp = new int[bagSize + 1];
+        dp[0] = 0;
+        for (int i = 0; i < weight.length; i++) {
+            for (int j = bagSize; j >= weight[i]; j--) {
+                if (j < weight[i]) continue;
+                dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
             }
         }
-        return dp[m - 1][n];
+        return dp[bagSize];
     }
 }
