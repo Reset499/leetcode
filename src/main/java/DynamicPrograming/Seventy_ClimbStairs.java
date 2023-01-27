3b4316cabbd4738b2ecae6d7ab5a2e99db5a853f
xxh205 <hello@itcast.cn>
2023-01-27 21:08:05 +0000

    leetcode

--- src/main/java/DynamicPrograming/Seventy_ClimbStairs.java
+++ src/main/java/DynamicPrograming/Seventy_ClimbStairs.java
@@ -6,11 +6,20 @@ public class Seventy_ClimbStairs {
     }
 
     public int climbStairs(int n) {
-        if (n < 2) return 1;
+//        if (n < 2) return 1;
+//        int[] dp = new int[n + 1];
+//        dp[0] = 1;
+//        dp[1] = 1;
+//        for (int i = 2; i < n + 1; i++) dp[i] = dp[i-1]+dp[i-2];
+//        return dp[n];
         int[] dp = new int[n + 1];
+        int[] weight = {1, 2};
         dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i < n + 1; i++) dp[i] = dp[i-1]+dp[i-2];
+        for (int j = 0; j <= n; j++) {
+            for (int i = 0; i < 2; i++) {
+                if(j>=weight[i]) dp[j]+=dp[j-weight[i]];
+            }
+        }
         return dp[n];
     }
 }
