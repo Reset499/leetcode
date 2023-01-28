595f887ed6ee4cb952d9f833e58946fd4bd57eac
xxh205 <hello@itcast.cn>
2023-01-28 15:28:43 +0000

    leetcode

---
+++ src/main/java/DynamicPrograming/OneHundredThirtyNine_WordBreak.java
@@ -0,0 +1,27 @@
+package DynamicPrograming;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class OneHundredThirtyNine_WordBreak {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] valid = new boolean[s.length() + 1];
+        valid[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i && !valid[i]; j++) {
+                if (set.contains(s.substring(j, i)) && valid[j]) {
+                    valid[i] = true;
+                }
+            }
+        }
+
+        return valid[s.length()];
+    }
+
+}
