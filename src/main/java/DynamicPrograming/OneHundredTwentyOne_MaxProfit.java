5a1867b32402b3de891accfd57ff5cbbed607f89
xxh205 <hello@itcast.cn>
2023-01-29 14:18:14 +0000

    leetcode

---
+++ src/main/java/DynamicPrograming/OneHundredTwentyOne_MaxProfit.java
@@ -0,0 +1,19 @@
+package DynamicPrograming;
+
+public class OneHundredTwentyOne_MaxProfit {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0 || prices == null) return 0;
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i-1][0],-prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0]+prices[i]);
+        }
+        return dp[prices.length-1][1];
+    }
+}
