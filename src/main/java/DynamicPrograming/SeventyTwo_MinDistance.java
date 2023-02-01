b13dec5cad86ab474680abfbbaefdcb4c0ccf4e5
xxh205 <masonhuo88@gmail.com>
2023-02-01 12:43:19 +0000

    leetcode

---
+++ src/main/java/DynamicPrograming/SeventyTwo_MinDistance.java
@@ -0,0 +1,19 @@
+package DynamicPrograming;
+
+public class SeventyTwo_MinDistance {
+    public static void main(String[] args) {
+
+    }
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length()+1][word2.length()+1];
+        for(int i = 1;i<word1.length()+1;i++) dp[i][0] = i;
+        for(int j = 1;j<word2.length()+1;j++) dp[0][j] = j;
+        for(int i = 1;i<word1.length()+1;i++){
+            for (int j =1;j<word2.length()+1;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]))+1;
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
