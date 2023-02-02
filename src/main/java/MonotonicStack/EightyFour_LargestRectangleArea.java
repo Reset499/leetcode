78090e1d9dc290c730c2b1b7b914e51725c0c150
xxh205 <masonhuo88@gmail.com>
2023-02-02 15:53:04 +0000

    leetcode

---
+++ src/main/java/MonotonicStack/EightyFour_LargestRectangleArea.java
@@ -0,0 +1,47 @@
+package MonotonicStack;
+
+import java.util.Stack;
+
+public class EightyFour_LargestRectangleArea {
+    public static void main(String[] args) {
+
+    }
+    int largestRectangleArea(int[] heights) {
+        Stack<Integer> st = new Stack<Integer>();
+
+        // 数组扩容，在头和尾各加入一个元素
+        int [] newHeights = new int[heights.length + 2];
+        newHeights[0] = 0;
+        newHeights[newHeights.length - 1] = 0;
+        for (int index = 0; index < heights.length; index++){
+            newHeights[index + 1] = heights[index];
+        }
+
+        heights = newHeights;
+
+        st.push(0);
+        int result = 0;
+        // 第一个元素已经入栈，从下标1开始
+        for (int i = 1; i < heights.length; i++) {
+            // 注意heights[i] 是和heights[st.top()] 比较 ，st.top()是下标
+            if (heights[i] > heights[st.peek()]) {
+                st.push(i);
+            } else if (heights[i] == heights[st.peek()]) {
+                st.pop(); // 这个可以加，可以不加，效果一样，思路不同
+                st.push(i);
+            } else {
+                while (heights[i] < heights[st.peek()]) { // 注意是while
+                    int mid = st.peek();
+                    st.pop();
+                    int left = st.peek();
+                    int right = i;
+                    int w = right - left - 1;
+                    int h = heights[mid];
+                    result = Math.max(result, w * h);
+                }
+                st.push(i);
+            }
+        }
+        return result;
+    }
+}
