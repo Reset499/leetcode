a52e680bf358c30811962b39cdabafc65b474022
xxh205 <hello@itcast.cn>
2023-02-10 22:59:02 +0000

    leetcode

---
+++ src/main/java/EveryDay/TwoThousandThreeHundredThrityFive_FillCups.java
@@ -0,0 +1,26 @@
+package EveryDay;
+
+import java.util.Arrays;
+
+public class TwoThousandThreeHundredThrityFive_FillCups {
+    public static void main(String[] args) {
+    }
+
+    //    public static int fillCups(int[] amount) {
+//        int result = 0;
+//        while (true) {
+//            Arrays.sort(amount);
+//            if (amount[2] == 0) return result;
+//            if (amount[1] != 0) amount[1]--;
+//            amount[2]--;
+//            result++;
+//        }
+//    }
+    public int fillCups(int[] amount) {
+        Arrays.sort(amount);
+        if (amount[2] >= amount[1] + amount[0]) {
+            return amount[2];
+        }
+        return (amount[0] + amount[1] + amount[2] + 1) / 2;
+    }
+}
