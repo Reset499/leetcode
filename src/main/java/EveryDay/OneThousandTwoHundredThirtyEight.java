23c51b30508b81cb105d4a66b05b1584a85d47e1
xxh205 <hello@itcast.cn>
2023-02-23 13:16:07 +0000

    leetcode

---
+++ src/main/java/EveryDay/OneThousandTwoHundredThirtyEight.java
@@ -0,0 +1,31 @@
+package EveryDay;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OneThousandTwoHundredThirtyEight {
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> circularPermutation(int n, int start) {
+        List<Integer> ret = new ArrayList<Integer>();
+        for (int i = 0; i < 1 << n; i++) {
+            ret.add((i >> 1) ^ i ^ start);
+        }
+        return ret;
+    }
+
+//    List<Integer> result = new ArrayList<>();
+//
+//    public List<Integer> circularPermutation(int n, int start) {
+//        result.add(start);
+//        backTracing(n,start);
+//        return result;
+//    }
+//
+//    public void backTracing(int n,int start) {
+//        if (result.size() == n) return;
+//        if(!result.contains()&&)
+//        result.remove(result.size()-1);
+//    }
+}
