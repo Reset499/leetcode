35235a87136abb7a0799ed938418b179797d9dd5
xxh205 <hello@itcast.cn>
2023-03-01 13:14:48 +0000

    leetcode

--- src/main/java/EveryDay/March/TwentyTwo_GenerateParenthesis.java
+++ src/main/java/EveryDay/March/TwentyTwo_GenerateParenthesis.java
@@ -5,55 +5,53 @@ import java.util.List;
 
 public class TwentyTwo_GenerateParenthesis {
     public static void main(String[] args) {
-
+        System.out.println(generateParenthesis(3));
     }
 
 
-//    public static List<String> generateParenthesis(int n) {
-//        int left = 0, right = 0;
-//        StringBuilder stringBuilder = new StringBuilder();
-//        backTracing(stringBuilder, left, right, n);
-//        return result;
-//    }
+    static List<String> result = new ArrayList<>();
 
-//    public static void backTracing(StringBuilder stringBuilder, int left, int right, int n) {
-//        if (left == n) {
-//            int curLen = stringBuilder.length();
-//            for (int i = curLen; i < 2 * n; i++) stringBuilder.append(")");
-//            result.add(stringBuilder.toString());
-//            stringBuilder.delete(curLen, 2 * n);
-//            return;
-//        }
-//        if (left == right) {
-//            stringBuilder.append("(");
-//            backTracing(stringBuilder, left + 1, right, n);
-//            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-//        }
-//        if(left>right) {
-//            stringBuilder.append(")");
-//            backTracing(stringBuilder, left, right + 1, n);
-//            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-//        }
-
-
-    // 做加法
-    List<String> result = new ArrayList<>();
-
-    public List<String> generateParenthesis(int n) {
-        if (n == 0) return result;
-        backTracing("", 0, 0, n);
+    public static List<String> generateParenthesis(int n) {
+        int left = 0, right = 0;
+        String s = "";
+        backTracing(s, left, right, n);
         return result;
     }
 
-    private void backTracing(String curStr, int left, int right, int n) {
-        if (left == n && right == n) {
-            result.add(curStr);
+    public static void backTracing(String s, int left, int right, int n) {
+        if (left == n) {
+            int curLen = s.length();
+            for (int i = curLen; i < 2 * n; i++) s+=")";
+            result.add(s);
             return;
         }
-        if (left < right) return;
-        if (left < n) backTracing(curStr + "(", left + 1, right, n);
-        if (right < n) backTracing(curStr + ")", left, right + 1, n);
+        if(left<right) return;
+        if (left < n) {
+            backTracing(s+"(", left + 1, right, n);
+        }
+        if (right < n) {
+            backTracing(s+")", left, right + 1, n);
+        }
     }
+
+
+//    // 做加法
+//
+//    public List<String> generateParenthesis(int n) {
+//        if (n == 0) return result;
+//        backTracing("", 0, 0, n);
+//        return result;
+//    }
+//
+//    private void backTracing(String curStr, int left, int right, int n) {
+//        if (left == n && right == n) {
+//            result.add(curStr);
+//            return;
+//        }
+//        if (left < right) return;
+//        if (left < n) backTracing(curStr + "(", left + 1, right, n);
+//        if (right < n) backTracing(curStr + ")", left, right + 1, n);
+//    }
 
 
 }
