b6a705388a3b2dd7b5298c952c2855f404c6665e
xxh205 <hello@itcast.cn>
2023-03-11 22:23:03 +0000

    leetcode

---
+++ src/main/java/EveryDay/March/FortyEight_Rotate.java
@@ -0,0 +1,21 @@
+package EveryDay.March;
+
+import javafx.scene.transform.Rotate;
+
+public class FortyEight_Rotate {
+    public static void main(String[] args) {
+        rotate(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
+    }
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length,level = n/2,temp = 0;
+        for(int i = 0;i<level;i++){
+            for(int j = i;j<n-1-i;j++){
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-i-1][n-1-j];
+                matrix[n-i-1][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i]  =temp;
+            }
+        }
+    }
+}
