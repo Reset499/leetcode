01c515ada69fa6c9cc2db50f40cb7ef50b6f5a22
xxh205 <hello@itcast.cn>
2023-03-13 16:49:31 +0000

    leetcode

---
+++ src/main/java/EveryDay/March/Fifty_MyPox.java
@@ -0,0 +1,17 @@
+package EveryDay.March;
+
+public class Fifty_MyPox {
+    public double myPow(double x, int n) {
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+
+    public double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+
+    }
+}
