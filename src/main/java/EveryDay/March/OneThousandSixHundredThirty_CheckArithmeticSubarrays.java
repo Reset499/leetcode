732cc30053c7e8b4abb54ab444bc68f1d1ef1dbb
xxh205 <hello@itcast.cn>
2023-03-22 22:22:41 +0000

    leetcode

--- src/main/java/EveryDay/March/OneThousandSixHundredThirty_CheckArithmeticSubarrays.java
+++ src/main/java/EveryDay/March/OneThousandSixHundredThirty_CheckArithmeticSubarrays.java
@@ -19,19 +19,20 @@ public class OneThousandSixHundredThirty_CheckArithmeticSubarrays {
     }
 
     public Boolean check(int[] nums, int left, int right) {
+        //gapNumber表示他们数组中有多少间隙
         int gapNumber = right - left, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        //找到最大最小的数在这个范围中
         for (int i = left; i <= right; i++) {
             min = Math.min(min, nums[i]);
             max = Math.max(max, nums[i]);
         }
+        //求出他们本应该的间隙距离 若除不净则直接return false 说明不成立
         if ((max - min) % gapNumber != 0) return false;
         int distance = (max - min) / gapNumber;
+        //若distance等于0,则说明都一样 直接return true
         if (distance == 0) {
-            for (int i = left; i <= right; i++) {
-                if (nums[i] != min) return false;
-            }
             return true;
-        } else {
+        } else {//检查每一个是否符合这个等差数列 并标记防止有重复的数字出现
             Set<Integer> set = new HashSet<>();
             for (int i = left; i <= right; i++) {
                 if ((nums[i] - min) % distance == 0 && !set.contains(nums[i])) {
