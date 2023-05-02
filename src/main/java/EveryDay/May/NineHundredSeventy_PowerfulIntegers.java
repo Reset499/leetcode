84636c4f662821566e78a457dfbf6fa40417226f
xxh205 <hello@itcast.cn>
2023-05-02 17:10:11 +0100

    leetcode

---
+++ src/main/java/EveryDay/May/NineHundredSeventy_PowerfulIntegers.java
@@ -0,0 +1,26 @@
+package EveryDay.May;
+
+import java.util.*;
+
+public class NineHundredSeventy_PowerfulIntegers {
+    public static void main(String[] args) {
+    }
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        List<Integer> result = new ArrayList<>();
+        int[] X = new int[20];
+        int[] Y = new int[20];
+        X[0] = 1;
+        Y[0] = 1;
+        for(int i = 1;i<20;i++){X[i] = X[i-1]*x;Y[i] = Y[i-1]*y;}
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0;i<20&&X[i]<=bound-1;i++){
+            for(int j = 0;j<20&&X[i]+Y[j]<=bound;j++){
+                if(!set.contains(X[i]+Y[j])){
+                    set.add(X[i]+Y[j]);
+                    result.add(X[i]+Y[j]);
+                }
+            }
+        }
+        return result;
+    }
+}
