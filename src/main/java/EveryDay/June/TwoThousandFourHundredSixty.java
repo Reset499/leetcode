93e0ed498094f678fc6d35930b0d2a9c392e6fb4
xxh205 <hello@itcast.cn>
2023-06-04 19:10:17 +0100

    leetcode

---
+++ src/main/java/EveryDay/June/TwoThousandFourHundredSixty.java
@@ -0,0 +1,29 @@
+package EveryDay.June;
+
+public class TwoThousandFourHundredSixty {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] applyOperations(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                right++;
+                left++;
+            } else {
+                right++;
+            }
+        }
+        return nums;
+    }
+}
