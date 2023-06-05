425a9b3b082018af9707d00546864d2a868711aa
xxh205 <hello@itcast.cn>
2023-06-05 22:53:52 +0100

    leetcode

---
+++ src/main/java/EveryDay/June/TwoThousandThreeHundredFiftyTwo.java
@@ -0,0 +1,31 @@
+package EveryDay.June;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoThousandThreeHundredFiftyTwo {
+    public static void main(String[] args) {
+
+    }
+
+    public int equalPairs(int[][] grid) {
+        int n = grid.length;
+        int result = 0;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                stringBuilder.append("-" + grid[i][j]);
+            }
+            String tmp = stringBuilder.toString();
+            map.put(tmp,map.getOrDefault(tmp,0)+1);
+        }
+        for(int j = 0;j<n;j++){
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0;i<n;i++) stringBuilder.append("-"+grid[i][j]);
+            String tmp = stringBuilder.toString();
+            result += map.containsKey(tmp)?map.get(tmp):0;
+        }
+        return result;
+    }
+}
